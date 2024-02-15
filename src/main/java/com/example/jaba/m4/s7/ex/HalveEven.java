3bba8135d658b44da84c41182c2e26c9f87b6368
Emanuele <egalli64@gmail.com>
2024-02-15 21:03:24 +0100

    Simplify and clarify

---
+++ src/main/java/com/example/jaba/m4/s7/ex/HalveEven.java
@@ -0,0 +1,19 @@
+/*
+ * Introduction to Java base concepts
+ * 
+ * https://github.com/egalli64/jaba
+ */
+package com.example.jaba.m4.s7.ex;
+
+/**
+ * Exercise on looping with while / do-while
+ */
+public class HalveEven {
+    public static void main(String[] args) {
+        int x = 64;
+        System.out.println("The initial value is " + x);
+
+        // TODO: if x is even, print it, halve it, and repeat
+        System.out.println("The expected result is: 64 32 16 8 4 2");
+    }
+}
---
+++ src/main/java/com/example/jaba/m4/s7/ex/NatualSum.java
@@ -0,0 +1,23 @@
+/*
+ * Introduction to Java base concepts
+ * 
+ * https://github.com/egalli64/jaba
+ */
+package com.example.jaba.m4.s7.ex;
+
+/**
+ * Exercise on loop with while / do-while
+ */
+public class NatualSum {
+    public static void main(String[] args) {
+        // The x value should be a natural number: 1, 2, ...
+        int x = 64;
+
+        System.out.println("The initial value is " + x);
+
+        // TODO: Calculate and print the sum of 1 + 2 + ... + x
+
+        // To check it, let's use the Gauss summation formula
+        System.out.println("The result should be " + (x * (x + 1) / 2));
+    }
+}
