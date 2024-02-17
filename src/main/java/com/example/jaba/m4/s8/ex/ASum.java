a1d22a620074c9372f94f43dc37d69e451e0932f
Emanuele <egalli64@gmail.com>
2024-02-17 12:34:53 +0100

    More loop exercises

---
+++ src/main/java/com/example/jaba/m4/s8/ex/ASum.java
@@ -0,0 +1,19 @@
+/*
+ * Introduction to Java base concepts
+ * 
+ * https://github.com/egalli64/jaba
+ * 
+ * Exercises on looping
+ */
+package com.example.jaba.m4.s8.ex;
+
+import java.util.Arrays;
+
+public class ASum {
+    public static void main(String[] args) {
+        // TODO: given a sequence of integers, generate the sum of each squared element
+        int[] values = { 1, 2, 3, 4, 5 };
+        System.out.println("Values is " + Arrays.toString(values));
+        System.out.println("The expected result is 55");
+    }
+}
---
+++ src/main/java/com/example/jaba/m4/s8/ex/BAge.java
@@ -0,0 +1,19 @@
+/*
+ * Introduction to Java base concepts
+ * 
+ * https://github.com/egalli64/jaba
+ * 
+ * Exercises on looping
+ */
+package com.example.jaba.m4.s8.ex;
+
+import java.util.Arrays;
+
+public class BAge {
+    public static void main(String[] args) {
+        // TODO: given a sequence of integers, calculate the average (as double)
+        int[] ages = { 21, 42, 37, 15 };
+        System.out.println("Ages are " + Arrays.toString(ages));
+        System.out.println("The expected result is 28.75");
+    }
+}
