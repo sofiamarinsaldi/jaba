6a42f1dd646a19fdd07ef9536ab4128865a507f4
Emanuele <egalli64@gmail.com>
2024-02-17 20:11:55 +0100

    StringBuilder exercises

---
+++ src/main/java/com/example/jaba/m5/s06/ex/Append.java
@@ -0,0 +1,18 @@
+/*
+ * Introduction to Java base concepts
+ * 
+ * https://github.com/egalli64/jaba
+ */
+package com.example.jaba.m5.s06.ex;
+
+/**
+ * Exercise to be solved using StringBuilder::append()
+ */
+public class Append {
+    public static void main(String[] args) {
+        // TODO: Generate a string from an array of strings
+        String[] data = { "a", "b", "c" };
+        System.out.println("The input is " + data);
+        System.out.println("Expecting something like \"a, b, c\" as output");
+    }
+}
---
+++ src/main/java/com/example/jaba/m5/s06/ex/Delete.java
@@ -0,0 +1,18 @@
+/*
+ * Introduction to Java base concepts
+ * 
+ * https://github.com/egalli64/jaba
+ */
+package com.example.jaba.m5.s06.ex;
+
+/**
+ * Exercise to be solved using StringBuilder::delete()
+ */
+public class Delete {
+    public static void main(String[] args) {
+        // TODO: Given a string with odd length, generate a copy without the central
+        // character, for example "abcde" => "abde"
+        String input = "abcde";
+        System.out.println("The input is " + input);
+    }
+}
