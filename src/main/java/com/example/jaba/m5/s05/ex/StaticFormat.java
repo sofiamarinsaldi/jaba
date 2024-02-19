2ac410cc391aec1ff64ac9fa3cb8c1e53871ee45
Emanuele <egalli64@gmail.com>
2024-02-19 08:56:19 +0100

    String format()

---
+++ src/main/java/com/example/jaba/m5/s05/ex/StaticFormat.java
@@ -0,0 +1,19 @@
+/*
+ * Introduction to Java base concepts
+ * 
+ * https://github.com/egalli64/jaba
+ */
+package com.example.jaba.m5.s05.ex;
+
+/**
+ * Exercise to be solved using String.format()
+ */
+public class StaticFormat {
+    public static void main(String[] args) {
+        // TODO: given an int and a double, put them in a string
+        int anInt = 42;
+        double aDouble = 3.14;
+        System.out.println("The input is: " + anInt + " and " + aDouble);
+        System.out.println("The expected output is something like \"42 and 3,14\"");
+    }
+}
