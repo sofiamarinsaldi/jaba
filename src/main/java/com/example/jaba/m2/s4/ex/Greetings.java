5b2bf09567f5f3cd83c046825a5051917c9533cd
Emanuele <egalli64@gmail.com>
2024-03-01 22:12:22 +0100

    moved to ex folder

--- src/main/java/com/example/jaba/m2/s4/ex/Greetings.java
+++ src/main/java/com/example/jaba/m2/s4/ex/Greetings.java
@@ -3,27 +3,30 @@
  * 
  * https://github.com/egalli64/jaba
  */
-package com.example.jaba.m2.s4;
+package com.example.jaba.m2.s4.ex;
 
 /**
  * Exercise on two-dimensional array
  */
-public class MatrixExercise {
+public class Greetings {
     /**
      * <ol>
-     * <li>Create a two line matrix
+     * <li>Some tasks on a two line matrix
      * <ul>
      * <li>Row 1: "ciao", "buongiorno"
      * <li>Row 2: "hello", "good morning"
      * </ul>
-     * <li>Print the first English greeting
-     * <li>Convert the Italian greetings in a string and print it
-     * <li>Convert the full matrix in a string and print it
      * 
      * @param args not used
      */
     public static void main(String[] args) {
-        // TODO
+        // TODO 1: create the two line matrix as specified above
+
+        // TODO 2: print the first English greeting
+
+        // TODO 3: convert the Italian greetings in a string and print it
+
+        // TODO 4: convert the full matrix in a string and print it
 
         System.out.println("Done");
     }
