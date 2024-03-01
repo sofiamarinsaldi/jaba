e36472d891a81a944f4bec7bebb71465e3606191
Emanuele <egalli64@gmail.com>
2024-03-01 22:06:49 +0100

    exercises

---
+++ src/main/java/com/example/jaba/m2/s3/ex/CopyOf.java
@@ -0,0 +1,22 @@
+/*
+ * Introduction to Java base concepts
+ * 
+ * https://github.com/egalli64/jaba
+ */
+package com.example.jaba.m2.s3.ex;
+
+import java.util.Arrays;
+
+/**
+ * Create array copy
+ */
+public class CopyOf {
+    public static void main(String[] args) {
+        int[] values = { 3, 6, 1, 3, 6 };
+        System.out.println("The original array is " + Arrays.toString(values));
+
+        // TODO 1: create and print a copy of the original array truncated to length 3
+
+        // TODO 2: create and print a copy of the original array expanded to length 10
+    }
+}
