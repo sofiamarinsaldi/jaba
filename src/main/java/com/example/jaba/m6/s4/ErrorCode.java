0c64638c7732a2e96edcbfc2da9139f1b17946f3
Emanuele <egalli64@gmail.com>
2024-07-23 12:25:35 +0200

    more details

--- src/main/java/com/example/jaba/m6/s4/ErrorCode.java
+++ src/main/java/com/example/jaba/m6/s4/ErrorCode.java
@@ -6,29 +6,37 @@
 package com.example.jaba.m6.s4;
 
 /**
- * Bad arguments - signaled by error code
+ * Bad argument signaled by error code
  */
 public class ErrorCode {
     /**
-     * When calling Math::sqrt() we should remember to check the returned value (if
+     * When calling Math.sqrt() we should remember to check the returned value (if
      * we are not dead sure about the argument). Otherwise we could have an
      * unexpected NaN in a variable.
      * 
      * @param args not used
      */
     public static void main(String[] args) {
-        // a bad argument
-        double argument = -1.0;
-        double radix = Math.sqrt(argument);
+        double[] arguments = { 42.0, 0.0, -1.0 };
 
-        // We should not use the returned value without checking it!
-        System.out.println("Square root is " + radix);
+        // !!! unsafe !!!
+        for (double arg : arguments) {
+            double radix = Math.sqrt(arg);
+            // We should not use the returned value without checking it!
+            System.out.println("Square root of " + arg + " is " + radix);
+        }
+        System.out.println();
 
-        // Check the return value before using it!
-        if (Double.isNaN(radix)) {
-            System.out.println("Can't calculate the square root of " + argument);
-        } else {
-            System.out.println("Square root is " + radix);
+        // safer
+        for (double arg : arguments) {
+            double radix = Math.sqrt(arg);
+
+            // Check the return value before using it!
+            if (Double.isNaN(radix)) {
+                System.out.println("Can't calculate the square root of " + arg);
+            } else {
+                System.out.println("Square root of " + arg + " is " + radix);
+            }
         }
     }
 }
