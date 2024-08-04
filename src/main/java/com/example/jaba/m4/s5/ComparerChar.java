eeccf4d185bc1597303f0456bdd9ee1548392823
Emanuele <egalli64@gmail.com>
2024-08-04 16:20:57 +0200

    readability

--- src/main/java/com/example/jaba/m4/s5/ComparerChar.java
+++ src/main/java/com/example/jaba/m4/s5/ComparerChar.java
@@ -16,23 +16,23 @@ public class ComparerChar {
      */
     public static void main(String[] args) {
         // letters are coded in the expected order
-        System.out.print("Is 'c' < 'k' (yes): ");
-        System.out.println('c' < 'k');
+        System.out.print("Is 'c' < 'k'? ");
+        System.out.println('c' < 'k'); // true
 
         // all uppercase letters are before all lowercase letters
-        System.out.print("Is 'Z' < 'a' (yes): ");
-        System.out.println('Z' < 'a');
+        System.out.print("Is 'Z' < 'a'? ");
+        System.out.println('Z' < 'a'); // true
 
         // ciphers are coded in the expected order
-        System.out.print("Is '0' < '9' (yes): ");
-        System.out.println('0' < '9');
-        
+        System.out.print("Is '0' < '9'? ");
+        System.out.println('0' < '9'); // true
+
         // ciphers are before letters
-        System.out.print("Is '0' (zero) < 'O' (upper 'o') (yes): ");
-        System.out.println('0' < 'O');
+        System.out.print("Is '0' (zero) < 'O' (upper 'o')? ");
+        System.out.println('0' < 'O'); // true
 
         // beware of characters that look similar!
-        System.out.print("Is 'ì' == 'i' (no): ");
-        System.out.println('ì' == 'i');
+        System.out.print("Is 'ì' == 'i': ");
+        System.out.println('ì' == 'i'); // false
     }
 }
